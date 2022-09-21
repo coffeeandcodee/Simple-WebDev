202dbd8393a058fa5e0b7a8b398efc5d24e064fd
coffeeandcodee <87856165+coffeeandcodee@users.noreply.github.com>
2022-09-21 13:22:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const colors = ["green","red", "blue", "yellow"]
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+     //get Random number between 0 and colors.length
+
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber);
+
+     document.body.style.backgroundColor = colors[randomNumber];
+     color.textContent = colors[randomNumber] 
+
+
+}) 
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
